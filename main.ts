743c64b9bf9bc89c19463bc7b5d4e2bc20764114
Code_Mac <80384615+MacVarint@users.noreply.github.com>
2021-12-14 14:25:53 +0100

    Changed the beaver texture

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 function beaver () {
-    _16360123162_6_0 = assets.image`a`
-    _16360123162_6_1 = assets.image`b`
-    _16360123162_6_2 = assets.image`c`
-    _16360123162_6_3 = assets.image`d`
+    Beaver2_0_0 = assets.image`Beaver0`
+    Beaver2_0_1 = assets.image`Beaver1`
+    Beaver2_0_2 = assets.image`Beaver2`
+    Beaver2_0_3 = assets.image`Beaver3`
 }
-let _16360123162_6_3: Image = null
-let _16360123162_6_2: Image = null
-let _16360123162_6_1: Image = null
-let _16360123162_6_0: Image = null
+let Beaver2_0_3: Image = null
+let Beaver2_0_2: Image = null
+let Beaver2_0_1: Image = null
+let Beaver2_0_0: Image = null
 beaver()
